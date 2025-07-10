9e26a428d13f08f9ced7dd5bb6bf85511fabd5fc
MrGeraldMayor <hisshass988@gmail.com>
2025-07-09 18:54:08 -0700

    Add files via upload

---
+++ Gerald.js
@@ -0,0 +1,100 @@
+//contact button
+function myButton() {
+    window.location.href = "contact.html";
+}
+
+//my first button
+function myFirstButton() {
+    window.location.href = "contact.html";
+}
+//my second button
+function mySecondButton() {
+    window.location.href = "contact.html";
+}
+//my fourth button
+function myFourthButton() {
+    window.location.href = "contact.html";
+}
+//my fifth button
+function myFifthButton() {
+    window.location.href = "contact.html";
+}
+//my sixth button
+function mySixthbutton(){
+    window.location.href = "contact.html";
+}
+//my seventh button
+function mySeventhButton(){
+    window.location.href = "contact.html";
+}
+//my eight button
+function myEightButton(){
+    window.location.href = "contact.html";
+}
+//my nine button
+function myNineButton(){
+    window.location.href = "contact.html";
+}
+//my ten button
+function myTenButton(){
+    window.location.href = "contact.html";
+}
+//my eleven button
+function myElevenButton(){
+    window.location.href = "contact.html";
+}
+//my tweleve button
+function myTwelveButton(){
+    window.location.href = "contact.html";
+}
+//my another one button
+function myAnotheroneButton(){
+    window.location.href = "contact.html";
+}
+//my another two button
+function myAnothertwoButton(){
+    window.location.href = "contact.html";
+}
+//my another three button
+function myAnotherthreeButton(){
+    window.location.href = "contact.html";
+}
+//my another four button
+function myAnotherFourthButton(){
+    window.location.href = "contact.html";
+}
+//my another fifth button
+function myAnotherFifthButton(){
+    window.location.href = "contact.html";
+}
+//my another sixth button
+function myAnotherSixthButton(){
+    window.location.href = "contact.html";
+}
+
+function myAnotherSeventhButton() {
+    window.location.href = "contact.html";
+}
+
+ function myAnotherEightButton() {
+    window.location.href = "contact.html";
+ }
+ function myAnotherNineButton() {
+    window.location.href = "contact.html";
+ }
+
+ function myAnotherTenButton() {
+    window.location.href = "contact.html";
+ }
+
+ function myAnotherElevenButton() {
+    window.location.href = "contact.html";
+ }
+
+ function myAnotherTwelveButton() {
+    window.location.href = "contact.html";
+ }
+
+ function myAnotherLastButton() {
+    window.location.href = "contact.html";
+ }
